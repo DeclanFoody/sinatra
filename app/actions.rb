163b2069f7870239a63d9768209f4ff417ae97f8
Declan Foody <charles.foody@mail.mcgill.ca>
2016-05-16 21:18:27 +0000

    commit

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,21 @@
 # Homepage (Root path)
-get '/messages' do
+get '/' do
+  erb :index
+end
+get'/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+  get '/messages/new' do
+  erb :'messages/new'
+end
+  post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  @message.save
+  redirect '/messages'
+end
+
